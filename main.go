02993b342b1bf64557ff15d18e7b01d59c047f71
baiyli <105086653+baiyli@users.noreply.github.com>
2022-10-18 15:40:48 -0700

    Update main.go

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
     "fmt"
     "net/http"
+    "log"
+    "os/exec"
 )
 
 func main() {
+    version, err := exec.Command("go", "version").Output()
+    if err != nil {
+        log.Fatal(err)
+    }
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "<h1>Welcome to App Runner!</h1>")
+        fmt.Fprint(w, string(version))
     })
     fmt.Println("Starting the server on :3000...")
     http.ListenAndServe(":3000", nil)
